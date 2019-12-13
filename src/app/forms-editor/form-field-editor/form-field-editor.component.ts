7f913d4738087a46c602138b11b5c043f47aedaf
tomaszociepa <tom.ociepa@gmail.com>
2019-12-13 17:33:47 +0100

    textarea select ngModel

--- src/app/forms-editor/form-field-editor/form-field-editor.component.ts
+++ src/app/forms-editor/form-field-editor/form-field-editor.component.ts
@@ -10,8 +10,20 @@ export class FormFieldEditorComponent implements OnInit {
   field_data = {
     name: 'Default value',
     active: true,
-    enabled: true
+    enabled: true,
+    hints: '',
+    type: {type: 'text', label:'Text Field'}
   }
+
+  compareType(type1, type2){
+    return type2 && type1.type == type2.type
+  }
+
+  fieldTypes = [
+    {type: 'text', label:'Text Field'},
+    {type: 'checkbox', label:'Checkbox Field'},
+    {type: 'select', label:'Select Field'}
+  ]
 
   constructor() { }
 
